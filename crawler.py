120bb776fde7c24a8bc29e3842f8e7fc8047a903
HyeM207 <hmk9667@gmail.com>
2023-04-05 20:04:50 +0900

    Feat: return dict in 'get_toon_info()'
    get_toon_info 함수에서 웹툰 정보를 가진 'webtoon_info' 딕셔너리를 리턴하여 리스트에 저장하도록 함

--- crawler.py
+++ crawler.py
@@ -35,38 +35,36 @@ def get_list_toonURL(driver, url) :
     return list_toonURL
     
 def get_toon_info(driver, toon_url) : 
+    webtoon_info = {}
+    
+    # 웹툰 정보 크롤링 
     driver.get(toon_url)
 
-    # 웹툰 정보 크롤링 
-    print("\n\n===============================================")
-    title_id = toon_url.split('=')[1] 
-    print(f'title_id : {title_id}')
-    title = driver.find_element(by=By.XPATH, value='//*[@id="ct"]/div[1]/div[1]/div[1]/strong')
-    print(f'title : {title.text}')
-    summary = driver.find_element(by=By.XPATH, value='//*[@id="ct"]/div[1]/div[1]/div[1]/span[1]')
-    print(f'summary : {summary.text}')
-    author = driver.find_element(by=By.XPATH, value='//*[@id="ct"]/div[1]/div[1]/div[1]/span[2]')
-    print(f'author : {author.text}')
-    score = driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_toon_info > div.info_front.is_active.anim_on > div.area_info > span.detail > span.score')
-    print(f'score : {score.text}')
-    count_num = driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_toon_info > div.info_front.is_active.anim_on > div.area_info > span.detail > span.favcount > span.count_num')
-    print(f'count_num : {count_num.text}')
-    
-    genre = driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_toon_info > div.info_back > dl > div.genre > dd > span').get_attribute('textContent')
-    print(f'genre : {genre}')
-    genre_detail = driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_toon_info > div.info_back > dl > div.genre > dd > ul > li').get_attribute('textContent')
-    print(f'genre_detail : {genre_detail}')
+    webtoon_info['title_id'] = toon_url.split('=')[1] 
+    webtoon_info['title'] = driver.find_element(by=By.XPATH, value='//*[@id="ct"]/div[1]/div[1]/div[1]/strong').text
+    webtoon_info['summary']  = driver.find_element(by=By.XPATH, value='//*[@id="ct"]/div[1]/div[1]/div[1]/span[1]').text
+    webtoon_info['author'] = driver.find_element(by=By.XPATH, value='//*[@id="ct"]/div[1]/div[1]/div[1]/span[2]').text
+    webtoon_info['score'] =  driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_toon_info > div.info_front.is_active.anim_on > div.area_info > span.detail > span.score').text
+    webtoon_info['count_num'] = driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_toon_info > div.info_front.is_active.anim_on > div.area_info > span.detail > span.favcount > span.count_num').text
+    webtoon_info['episode_count'] = driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_episode_count > h3 > span').text
+
+    # genre, genre_detail. week_days의 경우 'display:block'이라 .text로 접근 불가 -> get_attribute('textContent') 으로 데이터 가져옴
+    webtoon_info['genre'] = driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_toon_info > div.info_back > dl > div.genre > dd > span').get_attribute('textContent')
+    webtoon_info['genre_detail'] = driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_toon_info > div.info_back > dl > div.genre > dd > ul > li').get_attribute('textContent')
 
     week_days = []
     week_days_li = driver.find_elements(by=By.CSS_SELECTOR, value='#ct > div.section_toon_info > div.info_back > dl > div.week_day > dd > ul:nth-child(1) > li')
     for day in week_days_li : 
-        week_days.append(day.get_attribute('textContent'))
-    print(f'week_days : {week_days}')    
-    
-    episode_count = driver.find_element(by=By.CSS_SELECTOR, value='#ct > div.section_episode_count > h3 > span')
-    print(f'episode_count : {episode_count.text}')
+        week_days.append(day.get_attribute('textContent')) 
+    webtoon_info['week_days'] = week_days
+
+    return webtoon_info
 
     
+def get_comments(driver, toon_episode_url) : 
+    driver.get(toon_episode_url)
+
+
 if __name__ == "__main__" : 
     # 0. WebDriver 생성
     driver = create_driver()
@@ -96,10 +94,10 @@ if __name__ == "__main__" :
     with open('data.pickle', 'rb') as f : 
         total_toonURL = pickle.load(f)
 
-    # 2. 웹툰 별 url 접속하여 정보 가져오기
+    # 2. 웹툰 별 url 접속하여 정보 가져오기 (문제 : 19세 웹툰 접근 불가)
+    total_toon_info = []
     for toonURL in total_toonURL :
-        get_toon_info(driver, toonURL)
-
+        total_toon_info.append(get_toon_info(driver, toonURL))
 
     time.sleep(4) 
     driver.quit()
