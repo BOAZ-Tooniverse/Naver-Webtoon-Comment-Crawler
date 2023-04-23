fe19bd71929dcfb8e74a3ce38a4aff2283c0550d
HyeM207 <hmk9667@gmail.com>
2023-04-24 02:10:28 +0900

    Feat: Add 'send_slack_notification()' func to CrawlerError class

--- crawlerError.py
+++ crawlerError.py
@@ -1,4 +1,8 @@
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
 from config.save_path import ERROR_BEST_COMMENTS_TXT_FILE
+from config.slack_credential import SLACK_API_TOKEN, CHANNEL_NAME
 
 class CrawlerError(Exception):
     def __init__(self, message, title_id=None, epi_no=None):
@@ -6,10 +10,25 @@ class CrawlerError(Exception):
         self.title_id = title_id
         self.epi_no = epi_no
         self.log_error()
+        self.send_slack_notification()
+
 
     def log_error(self):
         with open(ERROR_BEST_COMMENTS_TXT_FILE, 'a') as f:
             f.write(f"{self}\n")
 
+
     def __str__(self):
         return f"{self.title_id}/{self.epi_no}"
+
+
+    def send_slack_notification(self):
+        client = WebClient(token=SLACK_API_TOKEN)
+        try:
+            response = client.chat_postMessage(
+                channel=CHANNEL_NAME,
+                text=f"CrawlerError occurred with message-Title ID: {self.title_id} Episode No: {self.epi_no} \n{super().__str__()}"
+            )
+            print(f"Slack Notification Sent: {response['ts']}")
+        except SlackApiError as e:
+            print("Slack Notification Failed: ", e.response["error"])
